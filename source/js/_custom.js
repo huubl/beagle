7dcba7d65a8cdb73a7066e989aff886dd1a61047
hungnv-0108 <hoang.van.hung@sun-asterisk.com>
2019-04-15 01:33:25 +0700

    Add sidebar right

--- source/js/_custom.js
+++ source/js/_custom.js
@@ -1,13 +1,19 @@
 $(function () {
+
+  // init bootstrap datetimepicker
   $(".datetimepicker").datetimepicker();
-  new PerfectScrollbar(document.querySelector(".js-list-scrollbar"), {
-    wheelPropagation: false
-  });
 
-  $(".ps__rail-y").css('height', $(".ps__rail-y").parents(".content").css("height"));
-  $(".ps__thumb-y").css('height', '165px')
+  // perfect scrollbar notifications
+  if ($(".js-list-scrollbar")[0]) {
+    new PerfectScrollbar(document.querySelector(".js-list-scrollbar"), {
+      wheelPropagation: false
+    });
+    $(".ps__rail-y").css('height', $(".ps__rail-y").parents(".content").css("height"));
+    $(".ps__thumb-y").css('height', '165px')
+  }
 
-  $(".multi-file").each(function () {
+  // input multifile file choose
+  $(".js-multi-file").each(function () {
     var t = $(this).next("label"),
       o = t.html();
     $(this).on("change", function (e) {
@@ -24,4 +30,35 @@ $(function () {
     })
   });
 
+  // toggle right sidebar
+  $(document).on('mouseup', function (e) {
+    e.preventDefault();
+    var container = $('.right-sidebar');
+    if (document.getElementsByClassName('js-toggle-right-sidebar')[0].contains(e.target)) {
+      $('body').toggleClass('open-right-sidebar');
+    }
+    else {
+      if (!container.is(e.target) && container.has(e.target).length === 0) {
+        $('body').removeClass('open-right-sidebar');
+      }
+    }
+  });
+
+  // toggle chat windows
+  $('.js-contact-link').each(function (index, element) {
+    $(this).click(function (e) {
+      e.preventDefault();
+      $('.tab-chat-inner').animate({
+        'left': '-290px'
+      }, 0);
+      $('.chat-windows').animate({
+        'left': '0'
+      }, 0)
+    });
+  });
+
+  $('.js-chat').click(function (e) {
+    e.preventDefault();
+    $('.tab-chat-inner, .chat-windows').removeAttr('style')
+  });
 });
